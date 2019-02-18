a54fd83117a86af46bb7016a7528f32a167cc697
alqaz <alqaz@outlook.com>
2019-02-18 17:41:14 +0800

    添加部分逻辑

---
+++ testuWS/main.cpp
@@ -0,0 +1,30 @@
+
+
+#include "uWS/uWS.h"
+
+#include <string>
+#include <string_view>
+#include <iostream>
+
+int main()
+{
+	using namespace uWS;
+	using namespace std;
+	uWS::Hub h;
+
+	h.onHttpRequest([](HttpResponse* resp, HttpRequest req, char* data, size_t len, size_t remain) {
+		string str{ "hello.world" };
+		resp->end(str.c_str(), str.length());
+		});
+	uS::Async _async(h.getLoop());
+	_async.start([](uS::Async* handle) {
+		cout << "hello.world" << endl;
+		});
+	h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+		ws->send(message, length, opCode);
+		});
+
+	if (h.listen("127.0.0.1",3000)) {
+		h.run();
+	}
+}
